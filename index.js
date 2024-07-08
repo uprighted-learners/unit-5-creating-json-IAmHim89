f073f585c3fbcefe3cc0f6c085c7f8d5a4d3eb2a
IAmHim89 <computertech0418@gmail.com>
2024-07-07 21:30:47 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const express = require('express');
+//express library
+const express = require("express");
+//variable holding the activation of express
 const app = express();
-
+//json file
+const api = "/answers.json";
+//port server #
 const PORT = 3000;
 
+//get response for json file/path
+app.get("/", (req, res) => {
+  res.send({ api });
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
